a35759ae333c9cf553484aae413a0f2aecdeac9a
binu47 <binis2000@gmail.com>
2025-03-03 20:11:02 -0800

    add file

---
+++ Hashset.java
@@ -0,0 +1,17 @@
+package stringhandling;
+import java.util.HashSet;
+
+public class Hashset {
+	public static void main(String[] args) {
+		
+		HashSet<String>hashSet= new HashSet<>();		
+		hashSet.add("Apple");
+		hashSet.add("Banana");
+		hashSet.add("apple");
+		
+		System.out.println("Hashset:  "  +  hashSet);
+		
+	
+	}
+
+}

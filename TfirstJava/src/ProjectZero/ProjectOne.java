740ec42d3f35d49cd061927757b42b846fefd7be
Yselam <yalewselam07@gmail.com>
2025-03-05 11:19:31 -0500

    new file

--- TfirstJava/src/ProjectZero/ProjectOne.java
+++ TfirstJava/src/ProjectZero/ProjectOne.java
@@ -4,7 +4,32 @@ public class ProjectOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// Inheritance: -- another OOP concept
 
-	}
+		public class A{
 
+		public void method1 (){
+		System.out.println("inside class A");
+		}
+
+		public void method2 (){
+		System.out.println("inside class A");
+		}
+
+		}
+
+		public class B extends A {
+
+		public void method3 (){
+		System.out.println("inside class A");
+		}
+		}
+
+		// A -parent
+		// B - child
+
+	
+		
+}
 }

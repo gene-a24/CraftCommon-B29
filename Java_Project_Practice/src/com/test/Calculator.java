a942f883485c1232d7207f1dce7dfadaf0ef54e4
craftgithubuser <craftknowledge.soresa@gmail.com>
2025-03-06 06:26:27 +0300

    Update Calculator.java

--- Java_Project_Practice/src/com/test/Calculator.java
+++ Java_Project_Practice/src/com/test/Calculator.java
@@ -5,12 +5,7 @@ public class Calculator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int value1 =  20, value2 = 40;
-		
-		int result =  value1 + value2;
-
-		
-		System.out.println("the addition: " + result);
+		int value1 =  20, v
 	}
 
 }
